dbf9778f0bd4b2a30e402f994b782455ea9dfcfc
Scvrlae <yurmepsycliv@gmail.com>
2025-08-22 21:52:19 +0200

    Scvrlae commit

---
+++ commands/ping.js
@@ -0,0 +1,28 @@
+/*
+
+	This code was written by Scvrlae (@datonescvrlae) and is his property. Unauthorized use and/or
+	distribution of this script is strongly prohibited! PLEASE ask first!
+
+	  ██████  ▄████▄ ██▒   █▓ ██▀███   ██▓    ▄▄▄      ▓█████ 
+	▒██    ▒ ▒██▀ ▀█▓██░   █▒▓██ ▒ ██▒▓██▒   ▒████▄    ▓█   ▀ 
+	░ ▓██▄   ▒▓█    ▄▓██  █▒░▓██ ░▄█ ▒▒██░   ▒██  ▀█▄  ▒███   
+	  ▒   ██▒▒▓▓▄ ▄██▒▒██ █░░▒██▀▀█▄  ▒██░   ░██▄▄▄▄██ ▒▓█  ▄ 
+	▒██████▒▒▒ ▓███▀ ░ ▒▀█░  ░██▓ ▒██▒░██████▒▓█   ▓██▒░▒████▒
+	
+	This file is part of the Ferron Discord bot owned and maintained by Scvrlae (@datonescvrlae).
+	It is private and not available for use under any circumstances!
+
+*/
+
+const {SlashCommandBuilder} = require("discord.js") // Mandatory discord.js shit
+
+module.exports = {
+       data: new SlashCommandBuilder()
+              .setName("ping")
+              .setDescription('Replies with "Pong" and the latency.'),
+
+       // The code that will be executed once the command is called
+       async execute(interaction) {
+              await interaction.reply("Pong!")
+       }
+}
